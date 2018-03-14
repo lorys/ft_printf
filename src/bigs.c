e9bd2df9d5a5e799d4ea1a2a4846aa379ccb1336
Lorys LOPEZ <llopez@e3r12p10.42.fr>
2018-03-14 16:41:01 +0100

    saves

--- src/bigs.c
+++ src/bigs.c
@@ -6,12 +6,21 @@
 /*   By: llopez <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/03/08 23:36:01 by llopez            #+#    #+#             */
-/*   Updated: 2018/03/09 00:56:01 by llopez           ###   ########.fr       */
+/*   Updated: 2018/03/14 16:35:02 by llopez           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 #include <locale.h>
+
+int					ft_putwchar(wchar_t c)
+{
+	if (c <= 0x7F)
+	{
+		ft_printf("%c", (int)c);
+	}
+	return (0);
+}
 
 int					ft_printf_bigc(const char *format, va_list ap, \
 		int *skip, t_arg *fg)
